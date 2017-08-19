e740e68b148e312c905e45909c39933a4e13249e
madicd <madicdjordje@gmail.com>
2017-08-19 21:24:51 +0200

    Added SgtPeppers CD implementation

---
+++ src/soundsystem/SgtPeppers.java
@@ -0,0 +1,14 @@
+package soundsystem;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc {
+
+    private String title = "Sgt. Pepper's Lonely hearts club band";
+    private String artist = "The Beatles";
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+    }
+}
