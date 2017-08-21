3bba4cd041bf50960546fe1a5b4330e3043fd9d9
madicd <madicdjordje@gmail.com>
2017-08-21 23:06:02 +0200

    Added test for CDPlayer.play

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -1,16 +1,25 @@
 package soundsystem;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Autowired
+    private MediaPlayer mediaPlayer;
 
     @Autowired
     private CompactDisc cd;
@@ -18,5 +27,11 @@ public class CDPlayerTest {
     @Test
     public void cdShouldNotBeNull() {
         assertNotNull(cd);
+    }
+
+    @Test
+    public void play() {
+        mediaPlayer.play();
+        assertEquals("Playing Sgt. Pepper's Lonely hearts club band by The Beatles\r\n", systemOutRule.getLog());
     }
 }
