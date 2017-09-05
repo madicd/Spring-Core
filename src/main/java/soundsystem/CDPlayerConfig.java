91fe5bb6fa458d2ccd5408c8205105a710237d53
Djordje Madic <madj@zuehlke.com>
2017-09-06 01:14:50 +0200

    Import disc title and artist from properties file

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,19 +1,30 @@
 package soundsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class CDPlayerConfig {
+
+    private final Environment env;
+
+    @Autowired
+    public CDPlayerConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public CompactDisc compactDisc() {
         BlankDisc blankDisc = new BlankDisc();
-        blankDisc.setArtist("The Beatles");
-        blankDisc.setTitle("Sgt. Pepper's Lonely hearts club band");
+        blankDisc.setArtist(env.getProperty("disc.artist"));
+        blankDisc.setTitle(env.getProperty("disc.title"));
         return blankDisc;
     }
 
