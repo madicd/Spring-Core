dda1c20a9ff3388b61d9d67b6e3b2b6316ce9f8b
Djordje Madic <madj@zuehlke.com>
2017-09-06 23:06:41 +0200

    Modified play() test

--- src/test/java/wiring/soundsystem/CDPlayerTest.java
+++ src/test/java/wiring/soundsystem/CDPlayerTest.java
@@ -1,5 +1,6 @@
 package wiring.soundsystem;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
@@ -9,9 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
@@ -40,8 +40,8 @@ public class CDPlayerTest {
     @Test
     public void play() {
         mediaPlayer.play();
-        String expectedContent = "Playing Sgt. Pepper's Lonely hearts club band by The Beatles\r\n";
-        assertEquals(expectedContent, systemOutRule.getLog());
+        String expectedContent = "Playing Sgt. Pepper's Lonely hearts club band by The Beatles";
+        assertThat(systemOutRule.getLog(), containsString(expectedContent));
     }
 
     @Test
