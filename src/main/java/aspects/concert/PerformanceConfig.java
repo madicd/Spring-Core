95b49518955eb71ad5577cda9582032345d3b781
Djordje Madic <madj@zuehlke.com>
2017-09-06 22:56:17 +0200

    Added JavaConfig for aspects

---
+++ src/main/java/aspects/concert/PerformanceConfig.java
@@ -0,0 +1,16 @@
+package aspects.concert;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan
+public class PerformanceConfig {
+    @Bean
+    public Audience audience() {
+        return new Audience();
+    }
+}
