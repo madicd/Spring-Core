19ff74d9041dd6dbb382ab13e6fb300a4ff6ed79
Djordje Madic <madj@zuehlke.com>
2017-09-06 22:51:30 +0200

    Defined pointcut for better reusability

--- src/main/java/aspects/concert/Audience.java
+++ src/main/java/aspects/concert/Audience.java
@@ -4,22 +4,26 @@ import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Audience {
-    @Before("execution(* aspects.concert.Performance.perform(..))")
+    
+    @Pointcut("execution(* aspects.concert.Performance.perform(..))")
+    public void performance() {}
+    
+    @Before("performance()")
     public void silenceCellPhones() {
         System.out.println("Silencing cell phones.");
     }
 
-    @Before("execution(* aspects.concert.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("Silencing cell phones.");
     }
 
-    @AfterReturning("execution(* aspects.concert.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause() {
         System.out.println("CLAP CLAP CLAP!!!");
     }
 
-    @AfterThrowing("execution(* aspects.concert.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Demanding refund.");
     }
