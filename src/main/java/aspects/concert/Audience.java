59281f75b7208b937fd7b6ecb120da829b9b4e11
Djordje Madic <madj@zuehlke.com>
2017-09-07 22:24:22 +0200

    Replaced advices with single around advice

--- src/main/java/aspects/concert/Audience.java
+++ src/main/java/aspects/concert/Audience.java
@@ -1,5 +1,6 @@
 package aspects.concert;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 @Aspect
@@ -7,24 +8,16 @@ public class Audience {
     
     @Pointcut("execution(* aspects.concert.Performance.perform(..))")
     public void performance() {}
-    
-    @Before("performance()")
-    public void silenceCellPhones() {
-        System.out.println("Silencing cell phones.");
-    }
 
-    @Before("performance()")
-    public void takeSeats() {
-        System.out.println("Silencing cell phones.");
-    }
-
-    @AfterReturning("performance()")
-    public void applause() {
-        System.out.println("CLAP CLAP CLAP!!!");
-    }
-
-    @AfterThrowing("performance()")
-    public void demandRefund() {
-        System.out.println("Demanding refund.");
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
+        try {
+            System.out.println("Silencing cell phones.");
+            System.out.println("Taking a seat.");
+            joinPoint.proceed();
+            System.out.println("CLAP CLAP CLAP!!!");
+        } catch(Throwable throwable) {
+            System.out.println("Demanding refund.");
+        }
     }
 }
