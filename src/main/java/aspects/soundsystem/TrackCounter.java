41b72be7c49bbc003461cbdd67a5841286acc094
Djordje Madic <madj@zuehlke.com>
2017-09-23 16:14:37 +0200

    Moved TrackCounter aspect configuration to xml

--- src/main/java/aspects/soundsystem/TrackCounter.java
+++ src/main/java/aspects/soundsystem/TrackCounter.java
@@ -1,21 +1,14 @@
 package aspects.soundsystem;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-
 import java.util.HashMap;
 import java.util.Map;
 
-@Aspect
 public class TrackCounter {
     private Map<Integer, Integer> trackCount = new HashMap<>();
 
-    @Pointcut("execution(* wiring.soundsystem.CompactDisc.playTrack(int)) && args(trackIndex)")
     public void trackPlayed(int trackIndex) {
     }
 
-    @Before(value = "trackPlayed(trackIndex)", argNames = "trackIndex")
     public void countTrack(int trackIndex) {
         int count = getPlayCount(trackIndex);
         trackCount.put(trackIndex, count + 1);
--- src/test/java/aspects/soundsystem/TrackCounterTest.java
+++ src/test/java/aspects/soundsystem/TrackCounterTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import wiring.soundsystem.CDPlayerConfig;
 import wiring.soundsystem.CompactDisc;
@@ -13,7 +14,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CDPlayerConfig.class)
+@ContextHierarchy({
+        @ContextConfiguration(classes = CDPlayerConfig.class),
+        @ContextConfiguration("classpath:track-counter-config.xml")
+})
 public class TrackCounterTest {
 
     @Autowired
